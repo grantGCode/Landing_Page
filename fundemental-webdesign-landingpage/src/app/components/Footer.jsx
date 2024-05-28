738777531374f9ebcc3d09df609e086f49d14680
Grant Bentley <gbentley0618@comcast.net>
2024-05-28 11:34:40 -0500

    add fontawsome mug-hot icon to footer

--- fundemental-webdesign-landingpage/src/app/components/Footer.jsx
+++ fundemental-webdesign-landingpage/src/app/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Form } from './Hero';
 import Image from 'next/image';
+import { FaMugHot } from "react-icons/fa";
 
 export const Footer = () => {
     const copy = `We just have one question for you. 
@@ -19,7 +20,7 @@ export const Footer = () => {
                     <Image className='footer-logo' src='footer-logo.svg' width={500} height={500}/>
                 </div>
                 <p className='credits'>
-                    A collaboration: Alvaro Castro Cid X Grant Bentley
+                <FaMugHot size={14}/> A collaboration: Alvaro Castro Cid X Grant Bentley
                 </p>
                 <div className='footer-image-container'>
                     <Image className='footer-logo' src='footer-text-logo.svg' width={500} height={500}/>
