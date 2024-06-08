66a8491684eb3b18d11b9d0c533617b5e3623df6
Grant Bentley <gbentley0618@comcast.net>
2024-06-08 13:52:19 -0500

    add alt tag to Fa mug icon

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -36,7 +36,7 @@ export const Footer = () => {
                     <Image className='footer-logo' src='footer-logo.svg' width={500} height={500} alt='footer-logo'/>
                 </div>
                 <p className='credits'>
-                <FaMugHot size={14}/> A collaboration: Alvaro Castro Cid X Grant Bentley
+                <FaMugHot size={14} alt='mug-icon'/> A collaboration: Alvaro Castro Cid X Grant Bentley
                 </p>
                 <div className='footer-image-container'>
                     <Image className='footer-logo' src='footer-text-logo.svg' width={500} height={500} alt='footer-logo' />
