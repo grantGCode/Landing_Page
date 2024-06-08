4a17fdfd268ddaf5170644fe8b01f4f6772ee109
Alvaro Castro Cid <alvarocastrocid@Alvaros-MacBook-Pro.local>
2024-06-08 16:19:50 -0400

    fixed footer issue;

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,48 +1,17 @@
-'use client'
-import { Form } from './Hero';
-import Image from 'next/image';
-import { useSearchParams } from "next/navigation"
-import { Suspense } from 'react';
+import Image from "next/image";
 
 export const Footer = () => {
-    const searchParams = useSearchParams();
-    const thankYou = searchParams.get("submitted");
-    const copy = `We just have one question for you. 
-        Are you ready to build your business dream? 
-        If so, sign up next to this text. 
-        We’ll reach out to set up a meeting.
-    `
-    const thankYouCopy = `We appreciate you’re interest
-        in working with us. We’ll reach out to you and
-        set up a call.
-    `
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            {thankYou ? 
-                <div className="footer-top-container">
-                    <p className='footer-copy'>{thankYouCopy}</p>
-                    <div className='thank-you-logo-wrapper'>    
-                        <Image className='footer-thank-you-logo' src='/footer-logo-and-thank-you.png' width={268} height={100} alt='thankyou' />
-                    </div>
-                </div>
-                :
-                <div className="footer-top-container">
-                    <p className='footer-copy'>{copy}</p>
-                    <Form/>
-                </div>
-            }
-            <div className='line'></div>
-            <div className='footer-bottom-container'>
-                <div className='footer-image-container'>
-                    <Image className='footer-logo' src='footer-logo.svg' width={500} height={500} alt='footer-logo'/>
-                </div>
-                <p className='credits'>
-                A collaboration: Alvaro Castro Cid X Grant Bentley
-                </p>
-                <div className='footer-image-container'>
-                    <Image className='footer-logo' src='footer-text-logo.svg' width={500} height={500} alt='footer-logo' />
-                </div>
+    return(
+        <div className='footer-bottom-container'>
+            <div className='footer-image-container'>
+                <Image className='footer-logo' src='footer-logo.svg' width={500} height={500} alt='footer-logo'/>
             </div>
-        </Suspense>
-    );
-}
+            <p className='credits'>
+            A collaboration: Alvaro Castro Cid X Grant Bentley
+            </p>
+            <div className='footer-image-container'>
+                <Image className='footer-logo' src='footer-text-logo.svg' width={500} height={500} alt='footer-logo' />
+            </div>
+        </div>
+    )
+};
