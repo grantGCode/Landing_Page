903f2f7c4e2d562034d5fd8f205fb7ea275c6cb6
Grant Bentley <gbentley0618@comcast.net>
2024-06-08 11:46:58 -0500

    save progress

--- fundemental-webdesign-landingpage/src/app/api/submitEmail/route.js
+++ fundemental-webdesign-landingpage/src/app/api/submitEmail/route.js
@@ -1,14 +1,14 @@
-export async function POST(req, res) {
+export async function POST(req) {
     const { email } = await req.json();
-    
     console.log({ email });
   
     if (!email) {
-      return new Responce(JSON.stringify({ error: 'Email is required' }), {
+      return new Response(JSON.stringify({ error: 'Email is required' }), {
         status: 400,
         headers: {'Content-Type': 'application/json'},
       });
-    }
+    };
+
     try {
       const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
       const API_KEY = process.env.MAILCHIMP_API_KEY;
@@ -20,7 +20,6 @@ export async function POST(req, res) {
 
       const response = await fetch(
         `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`,
-
         {
           body: JSON.stringify(data),
           headers: {
@@ -32,15 +31,25 @@ export async function POST(req, res) {
       );
 
       if (response.status >= 400) {
-        return res.status(400).json({
-          error: `There was an error submitting your info.
-          Please reach out at gbentley0618@gmail.com and will respond as soon as possible.`,
-      });
+        const errorData = await response.json();
+        return new Response(JSON.stringify({error: errorData.detail ||
+            `There was an error submitting your info. Please reach out 
+            at gbentley0618@gmail.com and will respond as soon as possible.`}), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json'},
+            });
+        } else {
+
+        return new Response(JSON.stringify({ message: 'Email submitted successfully' }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
     }
-  } catch (error) {
-    return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-}
+
+    } catch (error) {
+        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    };
+};
