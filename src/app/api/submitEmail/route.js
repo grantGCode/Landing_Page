a59958416d36981e9d4d3160dc9dcea9884f3d24
Grant Bentley <gbentley0618@comcast.net>
2024-11-01 01:39:57 -0500

    format route.js

--- src/app/api/submitEmail/route.js
+++ src/app/api/submitEmail/route.js
@@ -1,37 +1,39 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req,) {
-try {
+  try {
     const { email } = await req.json();
-    console.log({ email });
 
-      const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
-      const API_KEY = process.env.MAILCHIMP_API_KEY;
-      const DATACENTER = process.env.MAILCHIMP_API_SERVER;
-      
-      const data = {
-        email_address: email,
-        status: 'subscribed',
-      };
-
-      const response = await fetch(
-        `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`,
-        {
-          body: JSON.stringify(data),
-          headers: {
-            'Authorization': `apikey ${API_KEY}`,
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-        }
-      );
-      const newData = await response.json()
-      return NextResponse.json(newData)
-
-    } catch (error) {
-        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-            status: 500,
-            headers: { 'Content-Type': 'application/json' },
-        });
+    const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
+    const API_KEY = process.env.MAILCHIMP_API_KEY;
+    const DATACENTER = process.env.MAILCHIMP_API_SERVER;
+    
+    const data = {
+      email_address: email,
+      status: 'subscribed',
     };
+
+    const response = await fetch(
+      `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`,
+      {
+        body: JSON.stringify(data),
+        headers: {
+          'Authorization': `apikey ${API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      }
+    );
+      
+    const newData = await response.json()
+      
+    return NextResponse.json(newData)
+
+  } catch (error) {
+
+    return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  };
 };
